a25a03fef0aa7b5c52bd2880ac5a7d2b480433ca
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-06-01 23:47:14 +0800

    vars thing

--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,13 @@ const GSBlocks = require("content/GSBlocks");
 const GSUnitTypes = require("content/GSUnitTypes");
 const GSAchievements = require("content/GSAchievements");
 const GSSettings = require("content/GSSettings");
+const GSVars = require("GSVars");
 
 require("game/AchievementHandler");
 
 GSBlocks.load();
 GSUnitTypes.load();
+GSVars.load();
 
 Events.on(ClientLoadEvent, () => {
   GSAchievements.load();
