0748e25c78aa72f54c230021f1269bf1508a649e
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-06-01 20:31:28 +0800

    script yes

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,14 @@
 const GSBlocks = require("content/GSBlocks");
 const GSUnitTypes = require("content/GSUnitTypes");
+const GSAchievements = require("content/GSAchievements");
+const GSSettings = require("content/GSSettings");
+
+require("game/AchievementHandler");
 
 GSBlocks.load();
 GSUnitTypes.load();
+
+Events.on(ClientLoadEvent, () => {
+  GSAchievements.load();
+  GSSettings.load();
+});
