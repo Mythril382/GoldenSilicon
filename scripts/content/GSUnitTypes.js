4241b810d0489077c8ae980e4528f87d72ba7fa6
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-06-05 21:56:00 +0800

    gusamo

--- scripts/content/GSUnitTypes.js
+++ scripts/content/GSUnitTypes.js
@@ -1,11 +1,19 @@
 const {BulletAmmoType} = require("type/ammo/BulletAmmoType");
 const {BlockAmmoType} = require("type/ammo/BlockAmmoType");
 const {MineWeapon} = require("type/weapons/MineWeapon");
+const {TeleportBulletType} = require("entities/bullet/TeleportBulletType");
 
 exports.load = function() {
   //vanilla unit modifiers
   UnitTypes.reign.ammoType = new BulletAmmoType({});
   UnitTypes.dagger.ammoType = new BlockAmmoType(Blocks.copperWall, 15, {});
+  UnitTypes.dagger.weapons.each(w => {
+    w.bullet = new TeleportBulletType(2.5, 9, {
+      width: 7,
+      height: 9,
+      lifetime: 120
+    });
+  });
   UnitTypes.flare.ammoType = new BlockAmmoType(Blocks.copperWall, 15, {});
   UnitTypes.mega.weapons.add(new MineWeapon("repair-beam-weapon-center", {
     shootY: 6
